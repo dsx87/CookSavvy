f821bb28ee057dcc23ef06315286adfa852245d2
Igor Pivnyk <consul87@gmail.com>
2025-04-29 19:28:46 +0300

    Initial Commit

---
+++ CookSavvy/CookSavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookSavvyApp.swift
+//  CookSavvy
+//
+//  Created by Igor Pivnyk on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CookSavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
